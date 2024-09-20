680b9964b0d7681f5ae8ada5d9c4645f8c765166
Max Olofsson <max.olofsson@se.experis.com>
2024-09-20 15:40:44 +0200

    fizzbuzz completed

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,9 +2,21 @@ const answer = []
 
 // Write your code below this line
 
-
-
-
+for (let i = 1; i <= 15; ++i) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    answer.push('FizzBuzz')
+    continue
+  }
+  if (i % 3 === 0) {
+    answer.push('Fizz')
+    continue
+  }
+  if (i % 5 === 0) {
+    answer.push('Buzz')
+    continue
+  }
+  answer.push(i)
+}
 
 // Don't touch the code below this line, we'll cover it later
 module.exports = answer
